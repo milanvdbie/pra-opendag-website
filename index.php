e76fff5b4af5cd46a7c32c7f370ba2d4bd3cd31e
milanvdbie <milanvdbie@gmail.com>
2026-09-16 12:19:51 +0200

    text geschreven en vakanties toegevoegd

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
         <div class="welkom">
             <img src="img/Sanne Bas - Curio - Terheijdenseweg 350-2.jpg" alt="">
             <h2>Welkom</h2>
+            <p>Hallo je bent nu bij de opleiding software develeper. Hieronder staat wat informatie over de opleiding.</p>
         </div>
         
         <div class="vakkendeel">
@@ -23,19 +24,19 @@
             <div class="vakken">
                 <div class="vak">
                     <h3>Web</h3>
-                    <p>dit is een vak op deze opleiding</p>
+                    <p>Bij het vak web ga je leren programeren en specifiek leren hoe je websites kunt bouwen.</p>
                 </div>
                 <div class="vak">
                     <h3>Nederlands</h3>
-                    <p>dit is een vak op deze opleiding</p>
+                    <p>Je gaat nogsteeds nederlands krijgen op deze opleiding. In het eerste jaar ga je vooral focussen op lezen, luisteren en kijken. Je kan dan aan het einde klaar zijn voor het eerste nederlands examen dat gaat over lezen, luisteren en kijken.</p>
                 </div>
                 <div class="vak">
                     <h3>Rekenen</h3>
-                    <p>dit is een vak op deze opleiding</p>
+                    <p>Je krijgt ook nog rekenen (niet wiskunde). Hier kan kan best snel klaar mee zijn als je goed bent in rekenen. Je krijgt maar een examen voor reken en daarna is het klaar.</p>
                 </div>
                 <div class="vak">
                     <h3>Mentoruur</h3>
-                    <p>dit is een vak op deze opleiding</p>
+                    <p>Bij het mentoruur ga je met je klas en je mentor relevante informatie bespreken zoals hoe gaat het met huiswerk of als er binnenkort iets belangrijkks komt. Ook krijg je hier LOB daarbij ga je een paar opdrachten maken die nodig zijn om af te studeren.</p>
                 </div>
                 <div class="vak">
                     <h3>Burgerschap</h3>
@@ -43,23 +44,42 @@
                 </div>
                 <div class="vak">
                     <h3>Challenges</h3>
-                    <p>dit is een vak op deze opleiding</p>
+                    <p>Bij het vak challenges krijg je een project die of zelf om in groepen oet maken. Je hebt natuurlijk een deadline en je krijgt hier een punt voor.</p>
                 </div>
             </div>
             
         </div>
         <div class="calender">
-            <h2>Rooster</h2>
+            <h2>Dit is een voorbeeld van het soort rooster dat je kan verwachten</h2>
             <div class="rooster">
                 <img src="img/Rooster eerste leerjaar.png" alt="">
             </div>
+            <h2>Vakanties</h2>
+            <div class="vakantie">
+                <ul>
+                    <li>Curiodag (geen lessen)	1 oktober 2026</li>
+                    <li>Herfstvakantie	19 t/m 23 oktober 2026</li>
+                    <li>Kerstvakantie	21 december 2026 t/m 1 januari 2027</li>
+                    <li>Voorjaarsvakantie -  carnaval	8 t/m 12 februari 2027</li>
+                    <li>Goede vrijdag	26 maart 2027</li>
+                    <li>Tweede paasdag	29 maart 2027</li>
+                    <li>Meivakantie 	26 april t/m 7 mei 2027 (Koningsdag 27 april - bevrijdingsdag en hemelvaart 5 en 6 mei)</li>
+                    <li>Tweede pinksterdag	17 mei 2027</li>
+                    <li>Zomervakantie	26 juli t/m 3 september 2027</li>
+                </ul>
+            </div>
+            
+            
         </div>
     </main>
     <footer>
-        <h3>Contact</h3>
-        <p>Email: infopunt@curio.nl</p>
-        <p>Telefoonnummer: 088 209 80 00</p>
-        <p>Adress: Terheijdenseweg 350</p>
+        <div class="contact">
+            <h3>Contact</h3>
+            <p>Email: infopunt@curio.nl</p>
+            <p>Telefoonnummer: 088 209 80 00</p>
+            <p>Adress: Terheijdenseweg 350</p>
+        </div>
+        
     </footer>
 </body>
 </html>
